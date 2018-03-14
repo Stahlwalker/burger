ea9350e9a80140a89c27b877a45052613f13fb6e
Luke Stahl <Stahl1109@gmail.com>
2018-03-14 18:06:30 -0500

    image add

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 
-
 var express = require("express");
 var bodyParser = require("body-parser");
 
@@ -9,6 +8,7 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(express.static("public"));
+
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));

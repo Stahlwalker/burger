7b43c8f75bbe6588231f812f85f100161aba23e3
Luke Stahl <Stahl1109@gmail.com>
2018-03-14 17:37:44 -0500

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # burger
+
+[Friend Finder App](https://mighty-scrubland-37997.herokuapp.com/)
+
+## What the project does?
+    * This project is a node express app using mysql as a database and handlebars to serve the html page
+    * This is a single page like app where most the action happens on the same page  
+
+## How users can get started with the project?
+    * In the command line users can provide the following:
+    * 1. visit https://mighty-scrubland-37997.herokuapp.com/
+    * 2. follow the app which lets the user input the name of the burger
+    * 3. once burger name is submitted it will render on the left screen
+    * 4. if user clicks the devoured button it will move to the right screen. 
+    * 5. enjoy
+
+## Who maintains and contributes to the project?
+    * This project was created for a coding bootcamp and is maintained by the author Luke Stahl
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -10,6 +10,16 @@ router.get("/", function (req, res) {
   res.redirect("/burgers");
 })
 
+// router.get("/", function(req, res) {
+//   burger.selectAll(function(data) {
+//     var hbsObject = {
+//       burgers: data
+//     };
+//     console.log(hbsObject);
+//     res.render("index", hbsObject);
+//   });
+// });
+
 router.get("/burgers", function(req, res) {
   burger.selectAll(function(data) {
     var hbsObject = {
@@ -27,6 +37,32 @@ router.post("/burgers/add", function(req, res) {
   });
 });
 
+// router.post("/api/burgers", function(req, res) {
+//   burger.create(
+//     "burger_name", req.body.burger_name, function(result) {
+//     // Send back the ID of the new quote
+//     res.json({ id: result.insertId });
+//   });
+// });
+
+
+// router.put("/burgers/devour/:id", function(req, res) {
+//   var condition = "id = " + req.params.id;
+
+//   console.log("condition", condition);
+
+//   burger.update({
+//     devoured: req.body.devoured
+//   }, condition, function() {
+//     if (result.changedRows == 0) {
+//       // If no rows were changed, then the ID must not exist, so 404
+//       return res.status(404).end();
+//     } else {
+//       res.status(200).end();
+//     }
+//   });
+// });
+
 router.put("/burgers/devour/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
@@ -35,9 +71,11 @@ router.put("/burgers/devour/:id", function(req, res) {
   burger.update({
     devoured: req.body.devoured
   }, condition, function() {
-        res.redirect("/burgers");
+      res.redirect("/burgers");
   });
 });
+
+
 
 // Export routes for server.js to use.
 module.exports = router;

3a435ff7ae038e30bcd3dc0a4683108a1b282c2f
Luke Stahl <Stahl1109@gmail.com>
2018-03-14 18:34:20 -0500

    add method-override npm

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 
 var express = require("express");
 var bodyParser = require("body-parser");
+var methodOveride = require('method-override');
 
 // Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-app.use(express.static("public"));
+app.use(express.static(process.cwd() + '/public'));
 
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(methodOverride('_method'));
 
 var exphbs = require("express-handlebars");
 
